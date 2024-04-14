0b5fe3f7815c77f75230942a995a099187610e81
omerfaruk2626 <eseskikieses@gmail.com>
2024-04-15 00:20:51 +0300

    changed

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -1,13 +1,15 @@
+// HeaderNavbar.js
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import './Navbar.scss'; // Navbar bileşenine ait SCSS dosyasını import et
 
 function HeaderNavbar() {
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="https://github.com/omerfaruk2626" target="_blank" rel="noopener noreferrer" className="logo">O<span text-danger>F</span>S</Navbar.Brand>
+          <Navbar.Brand href="https://github.com/omerfaruk2626" target="_blank" rel="noopener noreferrer" className="logo">O<span class="text-danger">F</span>S</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/technologies">Technologies</Nav.Link>
@@ -16,8 +18,8 @@ function HeaderNavbar() {
           </Nav>
         </Container>
       </Navbar>
-      </>
+    </>
   );
 }
 
-export default HeaderNavbar
+export default HeaderNavbar;

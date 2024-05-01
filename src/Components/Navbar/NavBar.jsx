564a7b89d2f1f6c3d1313253658b05aa2349a1bf
omerfaruk2626 <eseskikieses@gmail.com>
2024-05-01 16:06:16 +0300

    navbar fixed

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -8,10 +8,10 @@ function HeaderNavbar() {
       <Navbar bg="dark" data-bs-theme="dark" >
           <Navbar.Brand href="/" className="logo">O<span class="text-danger">F</span>S</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/technologies">Technologies</Nav.Link>
-            <Nav.Link href="/projects">Projects</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link to="/">Home</Nav.Link>
+            <Nav.Link to="/technologies">Technologies</Nav.Link>
+            <Nav.Link to="/projects">Projects</Nav.Link>
+            <Nav.Link to="/contact">Contact</Nav.Link>
           </Nav>
       </Navbar>
     </>

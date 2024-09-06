2ae5d12d21b9c3f60bd1b0b5fb9af6afd19fcd01
Omer Faruk <154370292+omerfaruk2626@users.noreply.github.com>
2024-09-06 17:48:30 +0300

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
-import React from 'react'
+<html>
 
-const server = () => {
-  return (
-    <div>server</div>
-  )
-}
+<head>
 
-export default server
+<title> İlk HTML Sayfamız </title>
+
+</head>
+
+<body>
+
+<p size = "5" face = "Tahoma" color = "gray"> İlk HTML Sayfamız </p>
+
+</body>
+
+</html>

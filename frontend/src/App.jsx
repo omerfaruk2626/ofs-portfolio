4798e0a6d863a54e68622d8d3ff3c44150b85dc4
Omer Faruk <154370292+omerfaruk2626@users.noreply.github.com>
2025-01-14 19:20:53 +0300

    google tag manager

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { HashRouter as Router } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+import { useEffect } from "react";
 import ScrollToRoute from "./Components/ScrollToRoute";
 import Navbar from "./Components/Navbar";
 import Hero from "./sections/Hero";
@@ -8,12 +9,36 @@ import Technologies from "./sections/Technologies";
 import About from "./sections/About";
 import Contact from "./sections/Contact";
 import { LanguageProvider } from "./context/LanguageProvider";
+import { initGTM } from "./utils/gtm";
 import "./i18n";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    initGTM();
+
+    const noscript = document.createElement("noscript");
+    noscript.innerHTML = `
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PZHNXSM9"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    `;
+    document.body.insertAdjacentElement("afterbegin", noscript);
+  }, []);
+
   return (
     <HelmetProvider>
+      <Helmet>
+        <script>
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PZHNXSM9');
+          `}
+        </script>
+      </Helmet>
+
       <Router>
         <LanguageProvider>
           <Navbar />
